6b5c8bbb59e67ea0df7b0ad292ddfb6bff8f9bd3
WendellSphere <WendellSphere@users.noreply.github.com>
2018-01-18 07:36:50 -0800

    Update Utils.sql

--- Utils.sql
+++ Utils.sql
@@ -1,8 +1,29 @@
 /*
-        Various SQL stored precedures and functions for utility purposes
+        Various SQL stored precedures, triggers, and functions for utility purposes
         Authour: Will Wendell
 
 */
+----------------------------------------------------------------------------------------------------------------------------
+ALTER TRIGGER [dbo].[trg_NewProductEmail]
+   ON [dbo].[Product]
+   AFTER INSERT
+AS 
+BEGIN
+		DECLARE @productCD VARCHAR(20)
+
+		SELECT @productCD = I.sProduct_CD
+			FROM inserted I WHERE I.sProfit_Center_CD IN ('CBSPT', 'NUMIS') AND I.sCommodity_CD IN ('G','S','P','L') 
+
+		IF(@productCD IS NOT NULL)
+		BEGIN
+
+			EXEC NewProductEmail @productCD
+
+		END
+
+END
+
+----------------------------------------------------------------------------------------------------------------------------------------
 CREATE Proc [dbo].[CheckIfTablesAreEmtpy]
 AS
 BEGIN
@@ -13,7 +34,48 @@ END
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ALTER PROC [dbo].[NewProductEmail]
+	@productCD varchar(20)
+AS
+BEGIN
 
+			DECLARE @productDes VARCHAR(1000),
+					@productOZ VARCHAR(10)
+			SELECT 
+				@productDes = ISNULL(I.sProduct_Desc1, ISNULL(I.sProduct_Desc2 , ' ')),
+				@productOZ = CAST(I.decProduct_Ozconv AS VARCHAR(10))
+			FROM Product I WHERE I.sProduct_CD = @productCD
+
+		DECLARE @beginHTML VARCHAR(200) = '<!DOCTYPE html><html lang="en">',
+				@intialCD VARCHAR(200) = '<div>%s</div></br>',
+				@CD VARCHAR(200),
+				@intialDes VARCHAR(200) = '<div>%s</div></br>',
+				@Des VARCHAR(200),
+				@intialOZ VARCHAR(200) = '<div>OZ: %s </div></br>',
+				@OZ VARCHAR(200),
+				@recievers VARCHAR(500) = 'wwilliamwendell@gmail.com',
+				@bcc VARCHAR(500) = 'wwilliamwendell@gmail.com',
+				@subject VARCHAR(50);
+
+			EXEC xp_sprintf @CD OUTPUT, @intialCD, @productCD
+			EXEC xp_sprintf @Des OUTPUT, @intialDes, @productDes
+			EXEC xp_sprintf @OZ OUTPUT, @intialOZ, @productOZ
+			EXEC xp_sprintf @subject OUTPUT, 'New Product Code Created %s', @productCD
+
+
+			DECLARE	@body VARCHAR(1000) = @beginHTML + @CD + @Des + @OZ;
+			 
+
+			EXEC msdb.dbo.sp_send_dbmail @recipients = @recievers,
+										@blind_copy_recipients = @bcc,
+										--@from_address='',
+										@profile_name = NULL,
+										@subject = @subject,
+										@body = @body,
+										@body_format = 'HTML'
+
+END
+----------------------------------------------------------------------------------------------------------------------------------------
 CREATE PROC	[dbo].[EnableOrDisableConstrants]
 	@Disable BIT = 0,
 	@Enable	BIT = 0
